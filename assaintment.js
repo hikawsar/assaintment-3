4434db850178d1a7aa44bb8e73bc78e5e1104688
hikawsar <ahmedkawsarshamim@gmail.com>
2021-01-22 20:08:23 +0600

    first commit

---
+++ assaintment.js
@@ -0,0 +1,35 @@
+function kilometerToMeter (kilometer){
+var Meter = kilometer * 1000;
+return Meter;
+}
+var kawsarfeet = kilometerToMeter(1560);
+
+
+
+
+
+function budgetCalculator(taka){
+    var taka = 300 ;
+    var budget = 0 ;
+    if ( taka <= 50) {
+        budget = taka * 50;
+        
+    }
+    else if (taka <= 100) {
+        var firstPart = 50 * 10;
+        var remaining = taka - 50;
+        var secondPart = remaining / 100;
+        taka = firstPart + secondPart;
+    } 
+        else{
+            var firstPart = 50 * 10;
+            var secondPart = 50 / 500;
+            var remaining = taka - 100;
+            var thirdPart = remaining / 500;
+            taka = firstPart + secondPart + thirdPart;
+    }
+    return taka;
+}
+
+var count = budgetCalculator(588);
+console.log(count);
